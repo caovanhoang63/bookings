1ed0b97f0fa525a93eebc0239fc0b4df1f5e3bf6
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2023-09-24 11:05:51 +0700

    create relation models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,66 @@
 package models
 
+import (
+	"time"
+)
+
 // Reservation is present reservation form
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	AccessLevel int
+}
+
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	RoomID    int
+	StartDate time.Time
+	EndDate   time.Time
+	Room      Rooms
+}
+
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	RestrictionID int
+	Room          Rooms
+	Restriction   Restrictions
+	Reservation   Reservations
 }
